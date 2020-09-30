b1ea021b777f3b4f62820229922379a40b9e54f3
xy <785815707@qq.com>
2020-09-30 17:35:39 +0800

    保存到excel

--- get_douban_book.py
+++ get_douban_book.py
@@ -1,33 +1,38 @@
 '''
 爬取豆瓣书籍前250
+https://book.douban.com/top250
 '''
 
 import requests, time
 from bs4 import BeautifulSoup
-
-def select_book(url):
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-    }
-
-    res = requests.get(url, headers=headers)
-    soup = BeautifulSoup(res.text, 'html.parser')
-
-    a = soup.find_all('div', class_='pl2')
-    lis = []
-    for i in a:
-        tag = i.find('a')   # 遍历a标签
-        text = tag['title'] # 返回标签的文本信息
-        link = tag['href']  # 返回标签的其他元素
-        lis.append(text + " : " + link)
-    return lis
+from openpyxl import Workbook
 
 if __name__ == "__main__":
     url = 'https://book.douban.com/top250?start={}'
     urls = [url.format(i*25) for i in range(10)] 
 
-    for i in urls:
-        print(select_book(i))
-        print()
-        # 暂停2秒，防止访问太快被封
-        time.sleep(2)
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+
+    wb = Workbook()
+    sheet = wb.active
+    sheet.title = '书籍前250'
+    sheet.append(['书籍名称', '书籍链接'])
+
+    for url_go in urls:
+        res = requests.get(url_go, headers=headers)
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        a = soup.find_all('div', class_='pl2')
+        
+        for i in a:
+            tag = i.find('a')   # 遍历a标签
+            text = tag['title'] # 返回标签的文本信息
+            link = tag['href']  # 返回标签的其他元素
+            sheet.append([text, link])
+            print(text + " : " + link)
+
+        time.sleep(1)
+            
+    wb.save('豆瓣书籍前250.xlsx')
